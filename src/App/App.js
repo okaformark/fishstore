f228de6665c28521449d42c55a2a9a25b9f9b576
Mark Okafor <okaformark@gmail.com>
2019-06-24 18:53:58 -0500

    auth and home printing

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
+
+import Auth from '../components/Auth/Auth';
+
+import Home from '../components/Home/Home';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-info">Fish Store</button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Auth />
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Auth.scss';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Auth</h1>
+      </div>
+    );
+  }
+}
+export default Auth;
